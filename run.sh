673f1a878860016bfcbfb5fe60d2f40c0823e56f
neochrome <johan@stenqvist.net>
2015-12-21 07:35:41 +0100

    fixed broken container id retrieval

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ if [[ ! -e /docker.sock ]]; then
 	printf 'Cannot find /docker.sock, is it not mapped?' 1>&2
 	exit 1
 fi
-CONTAINER_ID=$(grep -o '[a-z,0-9]*$' /proc/self/cgroup | head -n 1)
+CONTAINER_ID=$(grep -Eom 1 '[a-f0-9]{64}' /proc/self/cgroup)
 
 docker inspect $CONTAINER_ID \
 	| jq '.[].Mounts|map(select(.Destination != "/docker.sock"))|map({share:.Destination|split("/")[-1],path:.Destination,writeable:.RW})' \
